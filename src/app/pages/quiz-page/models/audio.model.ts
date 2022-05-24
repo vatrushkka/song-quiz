f244dfe1e76323a9b1a992fb675686b2992cdd76
vatrushkka <valiakoziyra@gmail.com>
2022-05-25 02:12:11 +0300

    fixed project

---
+++ src/app/pages/quiz-page/models/audio.model.ts
@@ -0,0 +1,9 @@
+export interface AudioModel {
+  playing: boolean;
+  readableCurrentTime: string;
+  readableDuration: string;
+  duration: number | undefined;
+  currentTime: number | undefined;
+  canplay: boolean;
+  error: boolean;
+}
