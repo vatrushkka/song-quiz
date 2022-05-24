a0992962fb1d92a911191052da9c05d29e2dc715
vatrushkka <valiakoziyra@gmail.com>
2022-05-24 16:51:56 +0300

    added custom player styles

--- src/app/pages/quiz-page/components/quiz-player/quiz-player.component.ts
+++ src/app/pages/quiz-page/components/quiz-player/quiz-player.component.ts
@@ -3,6 +3,7 @@ import { SongData } from '../../../../shared/models/SongData.model';
 import { environment } from '../../../../../environments/environment.prod';
 import { DataService } from '../../../../shared/services/data.service';
 import { Subscription } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz-player',
@@ -13,15 +14,33 @@ export class QuizPlayerComponent implements OnInit {
   @Input() song: SongData;
   @Input() bgImg = false;
 
+
+  playing = false;
+  songLength = 30;
+  currentLength = 0;
+  currentProgress = 100 / this.songLength * this.currentLength;
   isAnswer: boolean;
   environment = environment;
+
+  value: FormControl;
 
   answerSub: Subscription;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.value = new FormControl(this.currentLength);
+
+    console.log(this.currentProgress);
     this.answerSub = this.dataService.isCorrectAnswer$.subscribe(val => this.isAnswer = val);
+
+    this.value.valueChanges.subscribe(val => {
+      this.currentProgress = 100 / this.songLength * val;
+    });
+  }
+
+  togglePlaying(): void {
+    this.playing = !this.playing;
   }
 
   ngOnDestroy(): void {
--- src/app/pages/quiz-page/quiz-page.module.ts
+++ src/app/pages/quiz-page/quiz-page.module.ts
@@ -4,6 +4,7 @@ import { QuizPageRoutingModule } from './quiz-page-routing.module';
 import { ButtonModule } from '../../shared/components/button/button.module';
 import { HeaderModule } from '../../shared/components/header/header.module';
 import { LoaderModule } from '../../shared/components/loader/loader.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { QuizPageComponent } from './quiz-page.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
@@ -21,13 +22,14 @@ import { SongInfoComponent } from './components/song-info/song-info.component';
     QuizPlayerComponent,
     SongInfoComponent
   ],
-    imports: [
-        CommonModule,
-        QuizPageRoutingModule,
-        ButtonModule,
-        HeaderModule,
-        LoaderModule
-    ],
+  imports: [
+    CommonModule,
+    QuizPageRoutingModule,
+    ButtonModule,
+    HeaderModule,
+    LoaderModule,
+    ReactiveFormsModule
+  ],
   exports: [
     QuizPageComponent
   ]
