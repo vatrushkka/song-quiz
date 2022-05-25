54ff591b93409571c8d91231025254deb989273a
vatrushkka <valiakoziyra@gmail.com>
2022-05-25 14:29:11 +0300

    fixed summary score

--- src/app/pages/summary-page/summary-page.component.ts
+++ src/app/pages/summary-page/summary-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { PlayerService } from '../../shared/services/player.service';
 import { Router } from '@angular/router';
+import { DataService } from '../../shared/services/data.service';
 
 @Component({
   selector: 'app-summary-page',
@@ -11,20 +12,30 @@ import { Router } from '@angular/router';
 export class SummaryPageComponent implements OnInit {
   name: string;
   score: number;
+  tabsAmount: number;
 
   subName: Subscription;
   subScore: Subscription;
+  tabsSub: Subscription;
 
-  constructor(private playerService: PlayerService, private router: Router) { }
+  constructor(
+    private playerService: PlayerService,
+    private router: Router,
+    private dataService: DataService
+  ) { }
 
   ngOnInit(): void {
     this.subName = this.playerService.player$.subscribe(name => {
       this.name = name;
-    })
+    });
 
     this.subScore = this.playerService.score$.subscribe(score => {
       this.score = score;
-    })
+    });
+
+    this.tabsSub = this.dataService.tabsAmount$.subscribe(amount => {
+      this.tabsAmount = amount;
+    });
   }
 
   playAgain(): void {
@@ -41,5 +52,6 @@ export class SummaryPageComponent implements OnInit {
   ngOnDestroy(): void {
     this.subName.unsubscribe();
     this.subScore.unsubscribe();
+    this.tabsSub.unsubscribe();
   }
 }
